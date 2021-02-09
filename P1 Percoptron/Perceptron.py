b17fc2535b6cb5b4d210ddb7ae48987011f078e5
WijnandVanDijk <60653370+WijnandVanDijk@users.noreply.github.com>
2021-02-09 21:03:26 +0100

    Verder gegaan in notebook

--- P1 Percoptron/Perceptron.py
+++ P1 Percoptron/Perceptron.py
@@ -2,16 +2,9 @@ import random
 
 class Perceptron:
 
-    def __init__(self):
-        self.weights = []
-        self.bias = None
-        self.inputs = []
-
-        for i in range(4):
-            self.inputs.append(random.randint(0, 1))
-
-        for i in range(4):
-            self.weights.append(random.random())
+    def __init__(self, bias, weight):
+        self.weights = weight
+        self.bias = bias
 
         def feed_forward(self):
             global som
@@ -20,11 +13,15 @@ class Perceptron:
                 som += self.weights[x] * self.inputs[x]
             return som # placeholder
 
-        feed_forward(self)
+        def activate(self):
+            if ((som*weight) + self.bias) < 0:
+                return 0
+            else:
+                return 1
 
-        if som > 0:
-            return 1
-        else:
-            return -1
+        def __str__(self):
+            return ('test')
 
-Perceptron()
+
+
+Perceptron(0, 0)
